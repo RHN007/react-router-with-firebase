f45bc70696984d2786edbe3016f2a3dffe08a3e1
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2022-04-14 10:34:35 +0600

    added onauthstate change to persist

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,19 +1,42 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAuth,GoogleAuthProvider,onAuthStateChanged,signInWithPopup, signOut } from "firebase/auth";
+import app from '../firebase.init'
+
+
+
+
+const auth = getAuth(app);
+
+const googleProvider = new GoogleAuthProvider()
 
 const useFirebase = () => {
     const [user, setUser] = useState({})
 
-
-
-
 const singInWithGoogle = () => {
-    console.log('singing in soon')
+   signInWithPopup(auth, googleProvider)
+  .then((result) => {
+    const user = result.user;
+    setUser(user)
+    console.log(user);
+  })
 }
 
+const handleSignOut = () =>  {
+        signOut(auth)
+        .then(() => {
+
+        })
+}
+
+useEffect(()=> {
+    onAuthStateChanged(auth, user => {
+        setUser(user);
+    })
+},[])
 
 
-// return [user, setUser]; 
-return {user, singInWithGoogle}
+
+return {user, singInWithGoogle, handleSignOut}
 
 
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../Hooks/useFirebase';
 import './Header.css'
 
 const Header = () => {
+  
+  const {user, handleSignOut} = useFirebase();
     return (
         <div className='header'>
             <nav>
@@ -10,7 +13,13 @@ const Header = () => {
                   <Link to='/products'>Products</Link>
                   <Link to='/orders'>Orders</Link>
                   <Link to='/register'>Register</Link>
-                <Link to='/login'>Login</Link>
+               <span> {user?.displayName && user.displayName}</span>
+               { 
+                    user?.uid ?
+                    <button onClick={handleSignOut}>SingOut</button>: 
+                   <Link to='/login'>Login</Link>
+               }
+                
 
 
             </nav>
